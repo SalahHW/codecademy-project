f6e5d15370e0ec52670c251d51b62030191df9f8
SalahHW <salah.bouheniche@gmail.com>
2021-12-28 20:54:57 +0100

    add function that choose random sentence part

---
+++ mixed-messages/main.js
@@ -0,0 +1,14 @@
+// Object that store multiple sentence parts in arrays
+const sentencePart = {
+  firstPart: [],
+  secondPart: [],
+  thirdPart: [],
+};
+
+/**
+ * Return a random element in the parameter array
+ * @param {} array 
+ */
+const randomElement = (array) => array[Math.floor(Math.random() * array.length)];
+
+
